902fd38f89c85b3ce525aa3fb8ecde6b107890a3
Viktor Richter <vrichter@posteo.de>
2017-02-02 16:11:05 +0100

    Person: add Stride typedef and cost functions

--- src/Person.cpp
+++ src/Person.cpp
@@ -16,15 +16,38 @@
 ********************************************************************/
 
 #include "Person.h"
+#include "Exception.h"
 #include <cmath>
 
 using fformation::Person;
 using fformation::Position2D;
 using fformation::Settings;
+using fformation::RotationRadian;
 
-Position2D Person::calculateTransactionalSegmentPosition(
-    Position2D::Coordinate stride) const {
+Position2D Person::calculateTransactionalSegmentPosition(Stride stride) const {
   return Position2D(
       _pose.position().x() + (stride * std::cos(_pose.rotation())),
       _pose.position().y() + (stride * std::sin(_pose.rotation())));
+}
+
+double Person::calculateVisibilityCost(const Position2D &group_center,
+                                       const Person &other) const {
+  if (_id == other.id()) {
+    return 0.; // same person
+  }
+  auto this_vector = group_center - pose().position();
+  auto this_distance = this_vector.norm();
+  auto other_vector = group_center - other.pose().position();
+  auto other_distance = other_vector.norm();
+  if (other_distance > this_distance) {
+    return 0.; // other is farther away from group than this
+  }
+  auto cosinus_angle =
+      this_vector.dot(other_vector) / (this_distance * other_distance);
+  if (cosinus_angle > _acos_of_theta) {
+    return 0.; // angle is too big
+  }
+  // exp(ln(k) * cos(\theta) * (d_i/d_j)) can be rearranged to k^(cos(\theta) *
+  // (d_i/d_j))
+  return pow(100, cosinus_angle * (this_distance / other_distance));
 }
--- src/Person.h
+++ src/Person.h
@@ -25,13 +25,70 @@ namespace fformation {
 
 class Person : public JsonSerializable {
 public:
+  /**
+   * @brief Stride the distance between a persons position and the center of its
+   * transactional space.
+   */
+  typedef Position2D::Coordinate Stride;
+
   Person(PersonId id, Pose2D pose) : _id(id), _pose(pose) {}
 
   const PersonId &id() const { return _id; }
   const Pose2D &pose() const { return _pose; }
 
-  Position2D
-  calculateTransactionalSegmentPosition(Position2D::Coordinate stride) const;
+  /**
+   * @brief calculateTransactionalSegmentPosition calculates the center of the
+   * transactional segment (TS) of the person.
+   *
+   * The transactional segment center of a person is assumed to be at the
+   * distance of stride along its viewing direction.
+   *
+   * @param stride the distance btw. a person and its TS. depends on the
+   * actually used unit of scale and overall 'crowdedness' of the scene.
+   * @return person-position + stride * viewing-direction
+   */
+  Position2D calculateTransactionalSegmentPosition(Stride stride) const;
+
+  constexpr static const RotationRadian visibilityAngleThreshold() {
+    return 0.75;
+  }
+
+  /**
+   * @brief calculateVisibilityCost the visibility-constraint-costs caused by
+   * another person.
+   *
+   * The visibility constraint can be calculated for an arbitrary group_center
+   * and person combination.
+   *
+   * Given \f$\theta_{i,j}^g\f$ being the angle btw. the persons relative to the
+   * passed group center, \f$d_i^g\f$ the distance of this and \f$d_j^g\f$ the
+   * distance oth ther to the group center, \f$\hat\theta = 0.75\f$ and
+   * \f$K=100\f$. This fuction calculates:
+   *
+   * \f$ R = 0 \f$ if \f$cos(\theta_{i,j}^g) < \hat\theta\f$ or \f$d_i^g <
+   * d_j^g\f$ and in all other cases:
+   *
+   * We use the constants \f$\hat\theta=acos(0.75)\f$ and \f$K=ln(100)\f$ and
+   * the Formula from the original matlab code:
+   *
+   * \f[ R = exp(K cos(\theta_{i,j}^g)) \frac{d_i^g}{d_j^g} \f]
+   *
+   * Instead of the original formula from the paper:
+   * \f[ R = exp(K cos(\theta_{i,j}^g)) \frac{d_i^g-d_j^g}{d_j^g} \f]
+   *
+   * @param group_center the assumed center of a group this person may be in.
+   * @param other the other person. not necessarily in the same group.
+   * @return the cost of the occlusion produced by other btw. this and
+   *         group_center.
+   *         * 0. if other == this
+   *         * 0. if other farther away from center than this
+   *         * 0. if the cos of the angle btw. this and other rel. to the center
+   *           is bigger than 0.75 radians.
+   *         * a cost value depending on the angles btw. the persons and their
+   *           distancces to the group center
+   */
+  double calculateVisibilityCost(const Position2D &group_center,
+                                 const Person &other) const;
 
   virtual void serializeJson(std::ostream &out) const override {
     out << "{ \"id\": " << _id;
@@ -42,6 +99,8 @@ public:
 private:
   const PersonId _id;
   const Pose2D _pose;
+  const double _acos_of_theta = 0.75;
+  const double _ln_of_k = 100;
 };
 
 } // namespace fformation
