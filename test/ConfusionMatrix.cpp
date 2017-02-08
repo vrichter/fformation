d7f2f812f3c5aaf27b69a75331608a654b52e1e1
Viktor Richter <vrichter@posteo.de>
2017-02-08 11:59:28 +0100

    ConfusionMatrix: add another test

--- test/ConfusionMatrix.cpp
+++ test/ConfusionMatrix.cpp
@@ -26,20 +26,35 @@
 namespace {
 typedef fformation::ConfusionMatrix<> ConfusionMatrix;
 
-TEST(ConfusionMatrixTest, CreateInstance) {
+static ConfusionMatrix random(){
+  return ConfusionMatrix(
+        (std::rand() + 1),
+        (std::rand() + 1),
+        (std::rand() + 1),
+        (std::rand() + 1));
+}
 
-  int tp = (std::rand() + 1) % 10000;
-  int fp = (std::rand() + 1) % 10000;
-  int tn = (std::rand() + 1) % 10000;
-  int fn = (std::rand() + 1) % 10000;
+static size_t mcMatrices() { return 10; }
+
+TEST(ConfusionMatrixTest, CreateInstance) {
+  int tp = 1;
+  int fp = 2;
+  int tn = 3;
+  int fn = 4;
   // general case
   ConfusionMatrix cm(tp, fp, tn, fn);
   EXPECT_EQ(cm.true_positive(), tp);
   EXPECT_EQ(cm.true_negative(), tn);
   EXPECT_EQ(cm.false_positive(), fp);
   EXPECT_EQ(cm.false_negative(), fn);
-  EXPECT_FLOAT_EQ(double(tp) / double(tp + fp), cm.calculatePrecision());
-  EXPECT_FLOAT_EQ(double(tp) / double(tp + fn), cm.calculateRecall());
+}
+
+
+TEST(ConfusionMatrixTest, PrecisionRecallCalculation) {
+  // general case
+  ConfusionMatrix cm = random();
+  EXPECT_FLOAT_EQ(double(cm.true_positive()) / double(cm.true_positive() + cm.false_positive()), cm.calculatePrecision());
+  EXPECT_FLOAT_EQ(double(cm.true_positive()) / double(cm.true_positive() + cm.false_negative()), cm.calculateRecall());
 
   // edge case 1
   cm = ConfusionMatrix(0, 0, 0, 0);
@@ -47,13 +62,40 @@ TEST(ConfusionMatrixTest, CreateInstance) {
   EXPECT_FLOAT_EQ(cm.calculateRecall(), 1.);
 
   // edge case 2
-  cm = ConfusionMatrix(0, 0, 0, fn);
+  cm = ConfusionMatrix(0, 0, 0, 1);
   EXPECT_FLOAT_EQ(cm.calculatePrecision(), 1.);
   EXPECT_FLOAT_EQ(cm.calculateRecall(), 0.);
 
   // edge case 2
-  cm = ConfusionMatrix(0, fp, 0, 0);
+  cm = ConfusionMatrix(0, 1, 0, 0);
   EXPECT_FLOAT_EQ(cm.calculatePrecision(), 0.);
   EXPECT_FLOAT_EQ(cm.calculateRecall(), 1.);
 }
+
+TEST(ConfusionMatrixTest, MeanCalculationsEquals) {
+  std::vector<ConfusionMatrix> equals;
+  ConfusionMatrix cm = random();
+  for (size_t i = 0; i < mcMatrices(); ++i){
+    equals.push_back(cm);
+  }
+  ASSERT_DOUBLE_EQ(cm.calculatePrecision(),ConfusionMatrix::calculateMeanPrecision(equals));
+  ASSERT_DOUBLE_EQ(cm.calculateRecall(),ConfusionMatrix::calculateMeanRecall(equals));
+}
+
+TEST(ConfusionMatrixTest, MeanCalculationsDifferent) {
+  std::vector<ConfusionMatrix> matrices;
+  for (size_t i = 0; i < mcMatrices(); ++i){
+    matrices.push_back(random());
+  }
+  double mean_precision = 0.;
+  double mean_recall = 0.;
+  for(auto mat : matrices){
+    mean_precision += mat.calculatePrecision();
+    mean_recall += mat.calculateRecall();
+  }
+  ASSERT_DOUBLE_EQ(mean_precision / double(matrices.size()), ConfusionMatrix::calculateMeanPrecision(matrices));
+  ASSERT_DOUBLE_EQ(mean_recall / double(matrices.size()), ConfusionMatrix::calculateMeanRecall(matrices));
+}
+
+
 }
