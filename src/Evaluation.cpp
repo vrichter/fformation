293ee681bbc736b0beb2473b8a0be1b3b18eb65a
Viktor Richter <vrichter@posteo.de>
2017-02-16 12:43:36 +0100

    Add Evaluation class

---
+++ src/Evaluation.cpp
@@ -0,0 +1,82 @@
+/********************************************************************
+**                                                                 **
+** File   : src/Evaluation.cpp                                     **
+** Authors: Viktor Richter                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+********************************************************************/
+
+#include "Evaluation.h"
+#include <assert.h>
+
+using fformation::Evaluation;
+using fformation::ConfusionMatrix;
+using fformation::Timestamp;
+using fformation::Classification;
+
+struct ThresholdValidator {
+  static bool validate(double t) { return (t >= 0. && t <= 1.); }
+};
+
+Evaluation::Evaluation(const Features &features,
+                       const GroundTruth &ground_truth,
+                       const Settings &settings, const GroupDetector &detector,
+                       const Options &options) {
+  // apply options
+  if (options.hasOption("threshold")) {
+    _threshold = options.getOption("threshold")
+                     .convertValue<double, ThresholdValidator>();
+  }
+  // do the evaluation
+  for (auto obs : features.observations()) {
+    const Classification *gt = ground_truth.findClassification(obs.timestamp());
+    if (gt != nullptr) {
+      _ground_truths.push_back(*gt);
+      _classifications.push_back(detector.detect(obs));
+      _confusion_matrices.push_back(
+          _classifications.back().createConfusionMatrix(*gt, _threshold));
+    }
+  }
+}
+
+static std::ostream &printMatlab(const Classification &cl, std::ostream &out) {
+  for (auto group : cl.idGroups()) {
+    if (group.persons().size() <= 1)
+      continue;
+    for (auto person : group.persons()) {
+      out << " " << person;
+    }
+    out << " |";
+  }
+  return out;
+}
+
+const std::ostream &Evaluation::printMatlabOutput(std::ostream &out) const {
+  assert(_classifications.size() == _confusion_matrices.size());
+  assert(_ground_truths.size() == _confusion_matrices.size());
+  for (size_t frame = 0; frame < _classifications.size(); ++frame) {
+    out << "Frame: " << frame + 1 << "/" << _classifications.size() << "\n";
+    out << "   FOUND:-- ";
+    printMatlab(_classifications[frame], out);
+    out << "\n";
+    out << "   GT   :-- ";
+    printMatlab(_ground_truths[frame], out);
+    out << "\n";
+  }
+  auto precision = ConfusionMatrix::calculateMeanPrecision(_confusion_matrices);
+  auto recall = ConfusionMatrix::calculateMeanRecall(_confusion_matrices);
+  out << "Average Precision: -- " << precision << "\n"
+      << "Average Recall: -- " << recall << "\n"
+      << "Average F1 score: -- "
+      << ConfusionMatrix::calculateF1Score(precision, recall) << std::endl;
+  return out;
+}
---
+++ src/Evaluation.h
@@ -0,0 +1,52 @@
+/********************************************************************
+**                                                                 **
+** File   : src/Evaluation.h                                       **
+** Authors: Viktor Richter                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+********************************************************************/
+
+#pragma once
+#include "ConfusionMatrix.h"
+#include "Features.h"
+#include "GroundTruth.h"
+#include "GroupDetector.h"
+#include "Options.h"
+#include "Settings.h"
+
+namespace fformation {
+
+class Evaluation {
+public:
+  Evaluation(const Features &features, const GroundTruth &ground_truth,
+             const Settings &settings, const GroupDetector &detector,
+             const Options &options = Options());
+
+  const std::vector<Classification> classifications() const {
+    return _classifications;
+  }
+  const std::vector<Classification> groundTruths() const {
+    return _ground_truths;
+  }
+  const std::vector<ConfusionMatrix> confusionMatrices() const {
+    return _confusion_matrices;
+  }
+  const std::ostream &printMatlabOutput(std::ostream &out) const;
+
+private:
+  double _threshold = 2. / 3.;
+  std::vector<Classification> _classifications;
+  std::vector<Classification> _ground_truths;
+  std::vector<ConfusionMatrix> _confusion_matrices;
+};
+
+} // namespace fformation
